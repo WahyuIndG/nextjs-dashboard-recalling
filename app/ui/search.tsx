5f1176e61a59c2a7697bfe888851f96c81244299
WahyuIndG <wahyuig03@gmail.com>
2024-09-16 12:49:28 +0700

    Chapter 11 | Search & Pagination
    page.tsx :
    Merupakan halaman Invoices, by default component Page di NextJs bisa menerima prop searchParams, di halaman ini fitur tersebut dimanfaatkan utk mendapatkan value 'query' dan 'page' yg dibutuhkan utk component InvioceTable & Pagination agar kemudian mereka dpt meng-fetch data yg diperlukan.
    
    search.tsx :
    Di component search, tidak menggunakan useState.
    Objective di component ini ialah :
    - mengubah URL sesuai dg pencarian yg user ketikan
    - mengirim permintaan pencarian otomatis menggunakan URL baru td dg fungsi replace() milik useRouter
    - mensinkronkan antara URL dg kolom Input dg memberi defaultValue={_relative trhdp URL_} pd elemen <input>
    
    pagination.tsx :
    Menambahkan logic utk generate URL utk masing-masing tombol paginasi (<-, 1, 2, 3, ..., ->).
    
    favicon.ico :
    Ngubah icon aja

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,18 +1,39 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  return (
-    <div className="relative flex flex-1 flex-shrink-0">
-      <label htmlFor="search" className="sr-only">
-        Search
-      </label>
-      <input
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder}
-      />
-      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-    </div>
-  );
+	const searchParams = useSearchParams(); // butuh dia utk get current URL (read-only)
+	const pathname = usePathname(); // butuh dia utk get current pathname
+	const { replace } = useRouter(); // butuh dia utk mengubah/mengupdate current URL yg aktif
+
+	const handleSearch = useDebouncedCallback((term) => {
+		console.log(`Searching... ${term}`);
+
+		const params = new URLSearchParams(searchParams);
+		params.set('page', '1'); // perlu utk mereset halaman ke halaman pertama disaat user malkukan pencarian. Kl nggak gitu, misal user lg d hal. 4 trus doing pencarian maka bisa jadi hal. 4 saat ini kosong krn banyaknya data yg didapat berdasarkan pencarian tadi tdk mencapai hal. 4
+		if (term) {
+			params.set('query', term);
+		} else {
+			params.delete('query');
+		}
+		replace(`${pathname}?${params.toString()}`);
+	}, 300);
+
+	return (
+		<div className="relative flex flex-1 flex-shrink-0">
+			<label htmlFor="search" className="sr-only">
+				Search
+			</label>
+			<input
+				className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+				placeholder={placeholder}
+				onChange={(e) => handleSearch(e.target.value)}
+				defaultValue={searchParams.get('query')?.toString()}
+			/>
+			<MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+		</div>
+	);
 }
